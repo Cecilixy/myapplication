38c38b32c72efebe698c12c76c6cb27b9154cb70
Xiaoxiao Yu <cecilixy@bu.edu>
2022-09-15 03:49:14 -0400

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button){
+            textView.setText("Hello World");
+        }
+    }
+}
